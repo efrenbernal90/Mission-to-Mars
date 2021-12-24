405057acffb495c2d3ebb1a9420bd6051beac5b0
Efren Bernal <efrenbernal90@gmail.com>
2021-12-23 17:16:52 -0800

    updated flask and script

--- Scraping.py
+++ Scraping.py
@@ -3,10 +3,26 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    # Set variables to be pulling data from function
+    news_title, news_paragraph = mars_news(browser)
+    
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
     # Visit the mars nasa news site
@@ -78,7 +94,9 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
     
-browser.quit()
+if __name__ == "__main__":
 
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Import tools for app
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+
 
 # Set up Flask
 app = Flask(__name__)
